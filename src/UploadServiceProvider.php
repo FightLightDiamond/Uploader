95f62e3d5ec45d5391d0bf9d25bccc24949aef5f
Pham Minh Cuong <phamminhcuong@Phams-MacBook-Pro.local>
2020-04-11 12:34:02 +0700

    1

--- src/UploadServiceProvider.php
+++ src/UploadServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Cuongpm\Uploader;
 
+use Cuongpm\Uploader\Facades\UploadFun;
 use Illuminate\Support\ServiceProvider;
 use Cuongpm\Uploader\Commands\MakeUploaderCommand;
 
@@ -17,6 +18,8 @@ class UploadServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/uploader.php' => config_path('uploader.php'),
         ]);
+
+        $this->app->bind('UploadFa', UploadFun::class);
     }
 
     /**
